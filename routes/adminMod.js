ce5c678e92ae6d4fce8dbc390164bd30e96b385c
RicardoVN <ricardovn@gmail.com>
2015-08-20 19:50:54 -0300

    redirect con link dinamico funcionando

--- routes/adminMod.js
+++ routes/adminMod.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET user-Mod. */
-router.get('/', function(req, res, next) {
-	res.render('adminMod', { css: './stylesheets/dashboard.css'});
+// router.get('/', function(req, res, next) {
+// 	res.render('adminMod', { css: './stylesheets/dashboard.css'});
+// });
+
+router.get('/:id', function (req, res, next) {
+	res.render('adminMod', { css: '/stylesheets/dashboard.css'});
+});
+
+
+//POST method route
+router.post('/', function (req, res) {
+	res.redirect('/admin');
 });
 
 
