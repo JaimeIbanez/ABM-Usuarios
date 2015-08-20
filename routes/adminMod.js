7d379e6c549a1f4940656e27728eb275fcb6ad51
RicardoVN <ricardovn@gmail.com>
2015-08-20 19:00:08 -0300

    trabajando en adminMod - todavía no funciona con id del user

--- routes/adminMod.js
+++ routes/adminMod.js
@@ -1,72 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var Mongo = require('mongodb').MongoClient;
-
-Mongo.connect('mongodb://localhost:27017/abm', function(err, db) {
-	if (err) {
-		throw new Error('Error: No hay conexión con la base de datos');
-	}
-
-	/* GET user-Mod. */
-	router.get('/:id', function(req, res) {
-		return users.findOne({ id: req.params.id }, function (err, post) {
-	    	if (err) { throw(err); }
-
-	    	return res.render('users', {users: post.users, url: post.URL /*, other data you need... */});
-  		});
-
-		//res.render('adminMod/:id', { css: './stylesheets/dashboard.css'});
-	});
-
-	// router.post('/', function(req, res, next) {
-	// 	var nameInput = req.body.inputNombre;
-	// 	var lastNameInput = req.body.inputApellido;
-	// 	var emailInput = req.body.inputEmail;
-	// 	var passwordInput = req.body.inputPassword;
-	// 	var photoInput = req.body.inputFoto;
-	// 	var phoneInput = req.body.inputTelefono;
-	// 	var birthdayInput = req.body.inputBirthday;
-
-	// 	console.log(nameInput, lastNameInput, emailInput, passwordInput, photoInput, phoneInput, birthdayInput);
-
-	// 	// calcular edad
-	// 	var bday = new Date(birthdayInput);
-	// 	var cur = new Date();
-	// 	var diff = cur-bday;
-	// 	var ageInput = Math.floor(diff/31536000000);
-
-
-	// 	db.collection('users').find({email: emailInput}).toArray(function(err, arr) {
-	// 		if (err || arr === undefined || arr.length === 0) {
-	// 			db.collection('users').find({} ,{id: 1}).sort({id: -1}).toArray(function(err, arr) {
-	// 				var lastId = arr[0].id;
-	// 				db.collection('users').insert({
-	// 					id: lastId+1,
-	// 					name: nameInput,
-	// 					lastName: lastNameInput,
-	// 					email: emailInput,
-	// 					password: passwordInput,
-	// 					phone: phoneInput,
-	// 					birthday: birthdayInput,
-	// 					age: ageInput,
-	// 					photo: photoInput,
-	// 					role: 'user'
-	// 				});
-	// 			});
-	// 		res.redirect('/admin');
-	// 		}
-	// 		else {
-	// 			// falta manejar mejor el error para que este dentro de la misma página
-	// 			res.render('error', {
-	// 				message: 'Ya existe un usuario registrado con ese email ' + emailInput + ' . Por favor registrarse con otro email',
-	// 	 			error: {status: 404, stack: new Error().stack}
-	// 	 		});
-	// 		}
-	// 	});
-
-
-	// });
+/* GET user-Mod. */
+router.get('/', function(req, res, next) {
+	res.render('adminMod', { css: './stylesheets/dashboard.css'});
 });
 
 
